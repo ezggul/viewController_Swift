2f9401bd10b1cbd21c5795e8dcbda49af81705e6
Ezgi Gül <ezgigul_>
2024-10-25 14:44:22 +0300

    Initial Commit

---
+++ viewControllerProjesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viewControllerProjesi
+//
+//  Created by Ezgi Gül on 25.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
